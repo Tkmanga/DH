fd8917b19718c2a6b877af6af05522190724df3f
Tkmanga <tacachojose@gmail.com>
2019-11-15 15:34:06 -0300

    apunte

---
+++ 2PHP7/php04/php4.php
@@ -0,0 +1,30 @@
+
+<?php
+//valiar
+//registrarlo
+
+  if ($_POST) {
+    header("location:exito.php");exit;
+  }
+ ?>
+ <!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="resultado.php" method="get">
+      <div class="">
+
+        <input type="text" name="nombre" value="">
+      </div>
+      <div class="">
+        <input type="text" name="email" value="">
+      </div>
+      <div class="">
+        <input type="submit" name="" value="Enviar">
+      </div>
+    </form>
+  </body>
+</html>
---
+++ 2PHP7/php04/resultado.php
@@ -0,0 +1,7 @@
+<?php
+  var_dump($_POST);
+  echo "<br>";
+  echo "hola ".$_POST["nombre"];
+  echo "<br>";
+  echo "tu mail es: ".$_POST["email"];
+?>
