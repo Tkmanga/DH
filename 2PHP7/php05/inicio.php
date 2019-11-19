57e5bc5b215f35d96e040155c6ad0adaebe8ae76
Jose Dario Tacacho <tacachojose@gmail.com>
2019-11-18 23:05:08 -0300

    php 05

---
+++ 2PHP7/php05/inicio.php
@@ -0,0 +1,50 @@
+
+
+<?php
+  if ($_FILES) {
+      if ($_FILES["cv"]["error"]!=0) {
+        echo "hubo un error al cargar el cv";
+      }else {
+          $ext = pathinfo($_FILES["cv"]["name"], PATHINFO_EXTENSION);
+          if ($ext !="pdf" && $ext != "doc" && $ext !="docx") {
+            echo "no admitimos esa extension de archivo para el cv";
+          }else {
+            move_uploaded_file($_FILES["cv"]["tmp_name"],"archivos/cv.".$ext);
+          }
+      }
+      if ($_FILES["imagen"]["error"]!=0) {
+          echo "hubo un error al cargar la imagen";
+      }else {
+          $ext = pathinfo($_FILES["imagen"]["name"], PATHINFO_EXTENSION);
+          if ($ext !="jpg" && $ext != "jpeg" && $ext !="png") {
+            echo "no admitimos esa extension de archivo para la imagen";
+          }else {
+            move_uploaded_file($_FILES["imagen"]["tmp_name"],"archivos/imagen.".$ext);
+          }
+      }
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <form class="" action="inicio.php" method="post" enctype="multipart/form-data">
+        <div class="">
+          <label for="">CV</label>
+          <br>
+          <input type="file" name="cv" value="">
+        </div>
+        <div class="">
+          <label for="">Imagen</label>
+          <br>
+          <input type="file" name="imagen" value="">
+        </div>
+        <br>
+        <input type="submit" name="" value="Enviar">
+      </form>
+  </body>
+</html>
