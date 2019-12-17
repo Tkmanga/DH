2bb5d63a055146eef4aa69d421e01d551b18ee62
Tkmanga <tacachojose@gmail.com>
2019-12-17 17:14:28 -0300

    asd asd

---
+++ 1HTML5CSS3/Practica online/Parte 5/prueba.php
@@ -0,0 +1,14 @@
+<?php
+$array = ["manzana","naranja","pera"];
+
+function pre($algo){
+  echo "<br>";
+  var_dump($algo);
+  echo "<br>";
+}
+
+function dd($unaCosa){
+  var_dump($unaCosa);
+  exit;
+}
+ ?>
